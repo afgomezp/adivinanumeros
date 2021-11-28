a2676b46cb5e5895b3f56c1a88242bd2333f6fd5
felipe gomez} <afgomezp@gmail.com>
2021-11-28 07:43:40 -0500

    se le agrega un sistema de 5 vidas las juego

--- adivina_numero.py
+++ adivina_numero.py
@@ -4,18 +4,26 @@ import random
 
 def run():
     numero_aleatorio = random.randint(1,100)
+    print ("vas a tener 5 intentos para adivinar el numero")
     numero_elegido = int(input("elige un numero del 1 al 100: "))
 
-    while numero_elegido != numero_aleatorio:
+    contador = 1
+    while contador <5:
         if numero_elegido < numero_aleatorio:
             print ('busca un numero más grande')
-        else:
+            numero_elegido =int(input('elige otro numero: '))
+
+        elif numero_elegido > numero_aleatorio:
             print ('Busca un numero más pequeño')
-        
-        numero_elegido =int(input('elige otro numero: '))
-    print ('Ganaste')
+            numero_elegido =int(input('elige otro numero: '))
+        else:
+            print ("ganaste")
+            contador =5
 
+        contador =contador + 1
 
+    print ("el juego terminó")
+   
 
 
     
