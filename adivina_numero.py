39ea48cbfedc318416a40d5fb882899794da58dd
afgomezp <95193225+afgomezp@users.noreply.github.com>
2021-12-01 19:41:47 -0500

    nuevo commit donde las vidas se suben a 10 en github
    se prueba dar mas vidas para adivinar más fácil los números.

--- adivina_numero.py
+++ adivina_numero.py
@@ -19,11 +19,11 @@ import random
 
 def run():
     numero_aleatorio = random.randint(1,80)
-    print ("vas a tener 5 intentos para adivinar el numero")
+    print ("vas a tener 10 intentos para adivinar el numero")
     numero_elegido = int(input("elige un numero del 1 al 80: "))
 
     contador = 1
-    while contador <5:
+    while contador <10:
         if numero_elegido < numero_aleatorio:
             print ('busca un numero más grande')
             numero_elegido =int(input('elige otro numero: '))
@@ -33,7 +33,7 @@ def run():
             numero_elegido =int(input('elige otro numero: '))
         else:
             print ("ganaste")
-            contador =5
+            contador =10
 
         contador =contador + 1
 
